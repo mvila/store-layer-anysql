91313dcfa9fea4364a29afcfb1bc59917de879ee
Manuel Vila <mvila@3base.com>
2015-11-12 15:55:58 +0900

    Add Cordova example

---
+++ example/cordova-app/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,14 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-sqlite-plugin/www/sqlite.js",
+        "id": "cordova-sqlite-plugin.SQLite"
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-sqlite-plugin": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ example/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+require('babel/polyfill');
+
+let log = window.log = function(message) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(message));
+  document.body.appendChild(div);
+};
+
+log('AnySQLKeyValueStore Example');
+
+window.addEventListener('error', function(err) {
+  log(err.message);
+}, false);
+
+import { assert } from 'chai';
+import AnySQLKeyValueStore from '../src';
+
+let store = new AnySQLKeyValueStore('cordova-sqlite:example');
+
+(async function() {
+  log('=== put, get and delete an object ===');
+  let key = ['users', 'mvila'];
+  await store.put(key, { firstName: 'Manu', age: 42 });
+  let user = await store.get(key);
+  log(JSON.stringify(user));
+  assert.deepEqual(user, { firstName: 'Manu', age: 42 });
+  let hasBeenDeleted = await store.delete(key);
+  assert.isTrue(hasBeenDeleted);
+  user = await store.get(key, { errorIfMissing: false });
+  assert.isUndefined(user);
+  hasBeenDeleted = await store.delete(key, { errorIfMissing: false });
+  assert.isFalse(hasBeenDeleted);
+})().catch(function(err) {
+  log(err.message);
+});
